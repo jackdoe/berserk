ed5cab1e7d2e51e8f01457ac1ed0a2c180d35473
borislav nikolov <jack@sofialondonmoskva.com>
2020-01-29 09:52:19 +0100

    allow talk

--- box/iptables.sh
+++ box/iptables.sh
@@ -2,6 +2,8 @@
 iptables -F OUTPUT
 ip6tables -F OUTPUT
 
+iptables -A OUTPUT -s 127.0.0.1/8 -d 127.0.0.1/8 -j ACCEPT
+
 for i in `seq 0 999`; do
     iptables -A OUTPUT -m owner --uid-owner $i -j ACCEPT
     ip6tables -A OUTPUT -m owner --uid-owner $i -j ACCEPT
